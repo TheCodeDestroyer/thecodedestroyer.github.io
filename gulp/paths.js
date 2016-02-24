2064706ae98a479e0894bd6682348bf2b7006fd9
Nace Logar <the.code.destroyer@gmail.com>
2016-02-24 09:22:57 +0100

    Changed back to vanilla structure and removed aurelia

--- gulp/paths.js
+++ gulp/paths.js
@@ -6,7 +6,7 @@ module.exports = {
     root: appRoot,
     source: appRoot + '**/*.js',
     html: appRoot + '**/*.html',
-    sass: appRoot + 'sass/**/*.scss',
+    sass: appRoot + 'sass/styles.scss',
     output: outputRoot,
     doc: './doc',
     gulp: gulpRoot + '**/*.js'
