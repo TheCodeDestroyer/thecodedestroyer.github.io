029ea27def390ff286184ac8398dd04012355324
Nace Logar <the.code.destroyer@gmail.com>
2016-03-10 19:53:12 +0100

    Created new task for injecting styles

---
+++ gulp/tasks/inject.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    inject = require('gulp-inject'),
+    paths = require('../paths');
+
+gulp.task('injectStyles', function () {
+    'use strict';
+
+    return gulp.src('./gulp/data/head.html')
+        .pipe(inject(gulp.src([
+                paths.temp + 'css/main.min.css',
+                paths.temp + 'css/libBundle.min.css'
+            ]),
+            {
+                starttag: '/*inject:theme:{{ext}}*/',
+                endtag: '/*endinject*/',
+                transform: function (filePath, file) {
+                    return file.contents.toString('utf8');
+                }
+            }))
+        .pipe(gulp.dest('./_includes/main'));
+});
