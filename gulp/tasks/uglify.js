f3d84dbe80a651c38cae82670f47f55651d9fc0d
Nace Logar <the.code.destroyer@gmail.com>
2016-03-10 20:15:03 +0100

    Changed tasks to bundle all js scripts into one

--- gulp/tasks/uglify.js
+++ gulp/tasks/uglify.js
@@ -10,27 +10,35 @@ gulp.task('minifyMain', function () {
     return gulp.src('./src/js/main.js')
     .pipe(rename('main.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.output + 'js'));
+    .pipe(gulp.dest(paths.temp + 'js'));
 });
 
 gulp.task('minifyJsLibs', function () {
     'use strict';
 
-    return gulp.src([
-        './jspm_packages/github/jquery/jquery@1.11.1/dist/jquery.js',
-        './jspm_packages/github/twbs/bootstrap@3.3.6/js/bootstrap.js',
-        './jspm_packages/github/gdsmith/jquery.easing@1.3.2/jquery.easing.js',
-        './jspm_packages/github/jquery/jquery-mousewheel@3.1.13/jquery.mousewheel.js',
-        './jspm_packages/github/TheCodeDestroyer/jquery-nicescroll@1.0.1/jquery.nicescroll.min.js',
-        './jspm_packages/github/TheCodeDestroyer/jquery-nicescroll@1.0.1/jquery.nicescroll.plus.js',
-        './jspm_packages/github/imakewebthings/jquery-waypoints@2.0.5/waypoints.js',
-        './jspm_packages/github/gilbitron/Nivo-Lightbox@1.2.0/nivo-lightbox.js',
-        './jspm_packages/github/stevenwanderski/bxslider-4@4.2.5/dist/jquery.bxslider.js',
-        './jspm_packages/github/jamesallardice/Placeholders.js@4.0.1/dist/placeholders.jquery.js',
-        './jspm_packages/github/crucifyer/css-browser-selector@master/css_browser_selector.js'
-    ])
+    return gulp.src('./jspm_packages/github/TheCodeDestroyer/jquery-nicescroll@1.0.1/jquery.nicescroll.plus.js')
     .pipe(uglify())
-    .pipe(concat('libBundle.min.js'))
+    .pipe(gulp.dest(paths.temp + 'js'));
+});
+
+gulp.task('concatJs', function () {
+    'use strict';
+
+    return gulp.src([
+        './jspm_packages/github/jquery/jquery@1.11.1/dist/jquery.min.js',
+        './jspm_packages/github/twbs/bootstrap@3.3.6/js/bootstrap.min.js',
+        './jspm_packages/github/gdsmith/jquery.easing@1.3.2/jquery.easing.min.js',
+        './jspm_packages/github/jquery/jquery-mousewheel@3.1.13/jquery.mousewheel.min.js',
+        './jspm_packages/github/TheCodeDestroyer/jquery-nicescroll@1.0.1/jquery.nicescroll.min.js',
+        paths.temp + 'js/jquery.nicescroll.plus.min.js',
+        './jspm_packages/github/imakewebthings/jquery-waypoints@2.0.5/waypoints.min.js',
+        './jspm_packages/github/gilbitron/Nivo-Lightbox@1.2.0/nivo-lightbox.min.js',
+        './jspm_packages/github/stevenwanderski/bxslider-4@4.2.5/dist/jquery.bxslider.min.js',
+        './jspm_packages/github/jamesallardice/Placeholders.js@4.0.1/dist/placeholders.jquery.min.js',
+        './jspm_packages/github/crucifyer/css-browser-selector@master/css_browser_selector.min.js',
+        paths.temp + 'js/main.min.js'
+    ])
+    .pipe(concat('main.min.js'))
     .pipe(gulp.dest(paths.output + 'js'));
 });
 
