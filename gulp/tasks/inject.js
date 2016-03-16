6df8c0437b59bdb42113a7be7848577ac389ceb8
Nace Logar <the.code.destroyer@gmail.com>
2016-03-16 13:07:17 +0100

    Changed the order of the css files

--- gulp/tasks/inject.js
+++ gulp/tasks/inject.js
@@ -7,8 +7,8 @@ gulp.task('injectStyles', function () {
 
     return gulp.src('./gulp/data/head.html')
         .pipe(inject(gulp.src([
-                paths.temp + 'css/main.min.css',
-                paths.temp + 'css/libBundle.min.css'
+                paths.temp + 'css/libBundle.min.css',
+                paths.temp + 'css/main.min.css'
             ]),
             {
                 starttag: '/*inject:theme:{{ext}}*/',
